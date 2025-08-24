ef24916eefd44e33a32dd7fefaa88b6aaba9d465
Mobeen Qamar <165556583+Mobeen-Dev@users.noreply.github.com>
2025-08-24 13:55:18 +0500

    tested-storefront-api

--- Shopifytest.py
+++ Shopifytest.py
@@ -7,39 +7,72 @@ import asyncio
 from config import NO_IMAGE_URL
 # @ App level create a reference for Shopify API client
 store = Shopify(settings.store, "ShopifyClient")
-def format_product(product: dict) -> dict:
-    """
-    Function to format the product data into a specific structure.
-    """
-    status = product.get("status")
-    print(f"Product Status: {status}")
-    if status == "ACTIVE":
-      edges = (product.get("media") or {}).get("edges") or []
-      first_edge = edges[0] if edges else {}
-      node = first_edge.get("node") or {}
-      image = node.get("image") or {}
-      image_url = image.get("url") or NO_IMAGE_URL
 
-      return {
-        "title": product.get("title", ""),
-        "handle": product.get("handle", ""),
-        "description": product.get("description", ""),
-        "vendor": product.get("vendor", ""),
-        "productType": product.get("productType", ""),
-        
-        "priceRange": {
-            "CurrencyCode": product.get("priceRangeV2", {}).get("maxVariantPrice", {}).get("currencyCode", ""),
-            "max_price": product.get("priceRangeV2", {}).get("maxVariantPrice", {}).get("amount", 0),
-            "min_price": product.get("priceRangeV2", {}).get("minVariantPrice", {}).get("amount", 0),
-        },
-        "totalInventory": product.get("totalInventory", 0),
-        "image_url": image_url,
-        "variants_options": product.get("options", []),
-    }
-    else:
-      return {
-        "Note": "This product is not active or available for sale at the moment."
+async def test():
+  
+  mutation = """
+    mutation {
+      cartCreate(
+        input: {lines: [{quantity: 1, merchandiseId: "gid://shopify/ProductVariant/50399177539862"}], buyerIdentity: {email: "example@example.com", countryCode: CA, deliveryAddressPreferences: {oneTimeUse: false, deliveryAddress: {address1: "150 Elgin Street", address2: "8th Floor", city: "Ottawa", province: "Ontario", country: "CA", zip: "K2P 1L4"}}, preferences: {delivery: {deliveryMethod: PICK_UP}}}, attributes: {key: "cart_attribute", value: "This is a cart attribute"}}
+      ) {
+        cart {
+          id
+          checkoutUrl
+          createdAt
+          updatedAt
+          lines(first: 10) {
+            edges {
+              node {
+                id
+                merchandise {
+                  ... on ProductVariant {
+                    id
+                  }
+                }
+              }
+            }
+          }
+          buyerIdentity {
+            deliveryAddressPreferences {
+              __typename
+            }
+            preferences {
+              delivery {
+                deliveryMethod
+              }
+            }
+          }
+          attributes {
+            key
+            value
+          }
+          cost {
+            totalAmount {
+              amount
+              currencyCode
+            }
+            subtotalAmount {
+              amount
+              currencyCode
+            }
+            totalTaxAmount {
+              amount
+              currencyCode
+            }
+            totalDutyAmount {
+              amount
+              currencyCode
+            }
+          }
+        }
+        userErrors {
+          field
+          message
+        }
       }
+    }
+  """
+  var = {}
+  return await store.send_storefront_mutation(mutation, var)
 
-print(format_product(asyncio.run(store.get_product_by_handle("line-following-robot-2wd-with-l298"))))
-
+print(asyncio.run(test()))
