5b84923e5c4733452af15ff0454350ea5fceeb48
Mobeen Qamar <mubeenqamar29@gmail.com>
2025-10-19 18:20:26 +0500

    added tests

--- test_chroma.py
+++
@@ -1,5 +0,0 @@
-import chromadb
-chroma_client = chromadb.HttpClient(host='localhost', port=8002)
-collection = chroma_client.get_collection(name="product_chunks")
-chroma_client.heartbeat()
-print(collection)
